73382fa0459c86119857a837fa6c703906294769
sergivna00 <sergivna00@gmail.com>
2020-04-20 19:11:40 +0300

    Add Algorithms library

--- SortingAlgo/Program.cs
+++ SortingAlgo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SortingAlgo
 {
@@ -6,7 +7,14 @@ namespace SortingAlgo
     {
         static void Main(string[] args)
         {
+            List<long> ls = new List<long>();
+            for (int i = 0; i < 1000000; i++)
+            {
+                ls.Add(3000000000);
+            }
+
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
